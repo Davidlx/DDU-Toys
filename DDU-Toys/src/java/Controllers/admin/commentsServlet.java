d75c49a4bd3dfe2f7c2243abd07a24f93b4826a1
Ugo El Koubi <eu201468@etu.unice.fr>
2016-04-13 12:07:40 +0800

    Merge origin/master
    Conflicts:
    	DDU-Toys/web/nav.jsp
    
    route fix

--- DDU-Toys/src/java/Controllers/admin/commentsServlet.java
+++ DDU-Toys/src/java/Controllers/admin/commentsServlet.java
@@ -36,7 +36,7 @@ public class commentsServlet extends basicServlet {
             throws ServletException, IOException, ClassNotFoundException, SQLException {
         response.setContentType("text/html;charset=UTF-8");
         request=super.retrieveBasicAttributes(request);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("management/comments.jsp"); 
+        RequestDispatcher dispatcher = request.getRequestDispatcher("comments.jsp"); 
         dispatcher.forward(request, response);
     }
 
